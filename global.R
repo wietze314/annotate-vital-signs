fc37f3497d06c1fdb1405ae2f2710301073f2937
Wietze Pasma <wietzepasma@gmail.com>
2019-05-09 15:33:34 +0200

    make alternative data generation possible

--- global.R
+++ global.R
@@ -26,78 +26,88 @@ vitalpalette <- paste0("#",vitaltypes$color)
 names(vitalpalette) <- vitaltypes$label
 
 
-getCaseData <- function(getCase = 1){
-  
+
+# data generation
+
+if(file.exists("functions/data.R"))
+{
+  source("functions/data.R")
+} else {
+  # use example data
+
   # example data is simulated
-  # this function should be replaced by a function that collects the actual data from the AIMS
+  # this function should be replaced by a function that collects the actual cases from the AIMS
   
-  set.seed(getCase)
+  procedures <- c('heart',
+                  'right leg',
+                  'left leg',
+                  'right arm',
+                  'levt arm',
+                  'right eye',
+                  'left eye',
+                  'abdomen',
+                  'brain')
+  ncase <- 100
   
-  vitals <- data.frame(time = 1:150,
-                       heartrate = round(rnorm(150,60, 5)),
-                       saturation = 100 - sample(0:9,
-                                                 150,
-                                                 replace = TRUE,
-                                                 prob = c(.8,.05,.05,.02,.02,.02,.01,.01,.01,.01))) %>%
-    gather(type, value, - time) 
+  allcaseinfo <- data.frame(id = 1:ncase,
+                            dos = as.POSIXct('2018-04-01 9:00') + 
+                              days(sample(-10:10,ncase, replace = T)) +
+                              hours(sample(0:8, ncase, replace = T)),
+                            procedure = paste('surgery on', sample(procedures,ncase, replace = T)))
   
-  vitalsnibp <- data.frame(time = 1:30*5-2,
-                           sysnibp = round(rnorm(30, 
-                                           120,
-                                           10))) %>%
-    mutate(dianibp = round(sysnibp - rnorm(30,50,5)),
-           meannibp = round((2 * dianibp+sysnibp)/3)) %>%
-    gather(type, value, - time) 
+  # get list of available dates
   
-  vitals %>%
-    bind_rows(vitalsnibp) %>%
-    mutate(case = getCase) %>%
-    # special display modus (local preference saturation on top in AIMS)
-    mutate(plotvalue = if_else(type == "saturation",3*value,value)) %>%
-    # simulate measurement identifier from data source
-    mutate(id = paste0(type, time)) %>%
-    mutate(time = as.POSIXct('2018-04-01 9:00') + minutes(time))
+  getDateList <- function(){
+    min <- allcaseinfo$dos %>% as.Date %>% min
+    max <- allcaseinfo$dos %>% as.Date %>% max
+    dat <- data.frame(dos = min + days(0:as.numeric(difftime(max,min),units = "days"))) %>%
+      left_join(allcaseinfo %>% mutate(dos = as.Date(dos)), by = 'dos') %>%
+      group_by(dos) %>%
+      summarise(n = sum(!is.na(id))) %>%
+      mutate(label = paste0(dos, " (",n,")"))
+    setNames(dat$dos, dat$label)
+  }
   
- 
+  getCaseList <- function(date){
+    dat <- allcaseinfo %>%
+      filter(as.Date(dos) == date)
+    setNames(dat$id, paste(dat$id, format(dat$dos,"%H:%M"), dat$procedure, sep = " - "))
+  }
+  
+  getCaseData <- function(getCase = 1){
+    
+    # example data is simulated
+    # this function should be replaced by a function that collects the actual data from the AIMS
+    
+    set.seed(getCase)
+    
+    vitals <- data.frame(time = 1:150,
+                         heartrate = round(rnorm(150,60, 5)),
+                         saturation = 100 - sample(0:9,
+                                                   150,
+                                                   replace = TRUE,
+                                                   prob = c(.8,.05,.05,.02,.02,.02,.01,.01,.01,.01))) %>%
+      gather(type, value, - time) 
+    
+    vitalsnibp <- data.frame(time = 1:30*5-2,
+                             sysnibp = round(rnorm(30, 
+                                                   120,
+                                                   10))) %>%
+      mutate(dianibp = round(sysnibp - rnorm(30,50,5)),
+             meannibp = round((2 * dianibp+sysnibp)/3)) %>%
+      gather(type, value, - time) 
+    
+    vitals %>%
+      bind_rows(vitalsnibp) %>%
+      mutate(case = getCase) %>%
+      # special display modus (local preference saturation on top in AIMS)
+      mutate(plotvalue = if_else(type == "saturation",3*value,value)) %>%
+      # simulate measurement identifier from data source
+      mutate(id = paste0(type, time)) %>%
+      mutate(time = as.POSIXct('2018-04-01 9:00') + minutes(time))
+    
+    
+  }
+  
 }
 
-# collect case data
-
-# example data is simulated
-# this function should be replaced by a function that collects the actual cases from the AIMS
-
-procedures <- c('heart',
-                'right leg',
-                'left leg',
-                'right arm',
-                'levt arm',
-                'right eye',
-                'left eye',
-                'abdomen',
-                'brain')
-ncase <- 100
-
-allcaseinfo <- data.frame(id = 1:ncase,
-                          dos = as.POSIXct('2018-04-01 9:00') + 
-                            days(sample(-10:10,ncase, replace = T)) +
-                            hours(sample(0:8, ncase, replace = T)),
-                          procedure = paste('surgery on', sample(procedures,ncase, replace = T)))
-
-# get list of available dates
-
-getDateList <- function(){
-  min <- allcaseinfo$dos %>% as.Date %>% min
-  max <- allcaseinfo$dos %>% as.Date %>% max
-  dat <- data.frame(dos = min + days(0:as.numeric(difftime(max,min),units = "days"))) %>%
-    left_join(allcaseinfo %>% mutate(dos = as.Date(dos)), by = 'dos') %>%
-    group_by(dos) %>%
-    summarise(n = sum(!is.na(id))) %>%
-    mutate(label = paste0(dos, " (",n,")"))
-  setNames(dat$dos, dat$label)
-}
-
-getCaseList <- function(date){
-  dat <- allcaseinfo %>%
-    filter(as.Date(dos) == date)
-  setNames(dat$id, paste(dat$id, format(dat$dos,"%H:%M"), dat$procedure, sep = " - "))
-}
